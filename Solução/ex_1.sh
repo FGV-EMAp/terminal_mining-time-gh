ae2cf970710d2873248dc37cf11282fb8966beb1
Guigas <cguilherme182@gmail.com>
2023-03-30 20:29:00 -0300

    ex_0

--- Solução/ex_1.sh
+++ Solução/ex_1.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-wc -l concatenado.txt
+#primeiro, como sugerido, eu concatenei os arquivos de texto por meio do comando cat
+#após isso, basta executar a linha a seguir
+
+wc -l concatenado.txt > resposta_ex_1
