b48cc1324c48a778d370e961bf09cf1008f7d979
Guigas <cguilherme182@gmail.com>
2023-03-29 22:47:46 -0300

    alterações no ex 1 e 2

---
+++ Solução/ex_2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+head -1 /home/guilherme/terminal_mining-time-gh/Dados/machado/*/*.txt
---
+++ Solução/ex_3.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sort -n head -1 /home/guilherme/terminal_mining-time-gh/Dados/machado/*/*.txt
