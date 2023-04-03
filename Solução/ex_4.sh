f2b92fe82384c6e7df8420428048d8ee2e9b8c68
HENRIQUE BORGES CARVALHO <B51984@fgv.edu.br>
2023-04-03 15:51:32 -0300

    códigos dos exercícios

--- Solução/ex_4.sh
+++ Solução/ex_4.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+cat ../Dados/machado/contos/macn001.txt | sed -E 's/[[:punct:]]* +/\n/g;s/.*/\L&/g' | sort | uniq -c | sort -n >resposta_ex_4.tx
--- Solução/ex_6.sh
+++ Solução/ex_6.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+cat resposta_ex_5.txt | grep -E "^ *[[:digit:]]{4}" >resposta_ex_6.txt
